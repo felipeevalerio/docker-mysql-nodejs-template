a9f54cf05ee22bb3cc9eb4ccfef2a3972fe5569b
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-10-26 11:34:06 -0300

    Trocando credenciais fixas para variáveis de ambiente

--- src/db.js
+++ src/db.js
@@ -1,10 +1,20 @@
 import { createPool } from 'mysql2';
+import dotenv from 'dotenv';
+import path from 'path';
+import {fileURLToPath} from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathENV = path.join(__dirname, '../process.env');
+dotenv.config({path: pathENV});
+
+console.log(process.env)
 const pool = createPool({
-    host: 'db',
-    port: 3306,
-    password: 'root',
-    user: 'root',
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    password: process.env.MYSQL_PASSWORD,
+    user: process.env.MYSQL_USER,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
